1c2244978ac015209e29f22e7cac857e108f734c
59160050 <59160050@go.buu.ac.th>
2019-12-23 15:15:37 +0700

    add menu

--- client/js/disable.js
+++ client/js/disable.js
@@ -1,3 +1,7 @@
 function disFunction() {
-    document.getElementById("disable").disabled = false;
+  document.getElementById("disable").disabled = false;
+}
+
+function resetFunction() {
+  document.getElementById("form9").reset();
 }
