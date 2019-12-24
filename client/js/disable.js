cd982e4b9294bb33390b763f2f81da32582a1ddb
59160050 <59160050@go.buu.ac.th>
2019-12-24 09:52:45 +0700

    reset

--- client/js/disable.js
+++ client/js/disable.js
@@ -1,7 +1,7 @@
 function disFunction() {
   document.getElementById("disable").disabled = false;
-}
-
-function resetFunction() {
   document.getElementById("form9").reset();
 }
+
+// function resetFunction() {
+// }
