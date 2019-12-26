bb0388a37666918b67428efa6cc6d27cc8931c8d
heroserth2 <59160969@go.buu.ac.th>
2019-12-26 15:19:24 +0700

    control search

--- client/js/date/dateth.js
+++ client/js/date/dateth.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     $('.datepicker').datepicker({
         format: 'dd/mm/yyyy',
-        todayBtn: true,
         language: 'th',             //เปลี่ยน label ต่างของ ปฏิทิน ให้เป็น ภาษาไทย   (ต้องใช้ไฟล์ bootstrap-datepicker.th.min.js นี้ด้วย)
         thaiyear: true              //Set เป็นปี พ.ศ.
     }).datepicker("setDate", "0");  //กำหนดเป็นวันปัจุบัน
