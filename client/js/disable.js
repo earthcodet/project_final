57e011b452ba1de0ed94fcfb9d4366eec51bd3c9
59160050 <59160050@go.buu.ac.th>
2020-01-03 13:17:44 +0700

    comment js

--- client/js/disable.js
+++ client/js/disable.js
@@ -2,16 +2,16 @@ function disFunction() {
   document.getElementById("disable").disabled = false;
   
   // reset
-  document.getElementById("form").reset();
+  // document.getElementById("form").reset();
 }
 
-function enableFunction() {
-  document.getElementById("disable").disabled = true;
-}
+// function enableFunction() {
+//   document.getElementById("disable").disabled = true;
+// }
   
-function disableFunction() {
-  document.getElementById("disable").disabled = false;
-}
+// function disableFunction() {
+//   document.getElementById("disable").disabled = false;
+// }
 // function resetFunction() {
 // }
 
