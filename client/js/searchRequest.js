e72fe57244dc1df094c01617bb4379fce05305f1
59160050 <59160050@go.buu.ac.th>
2020-01-10 09:39:51 +0700

    1/9 1/10

--- client/js/searchRequest.js
+++ client/js/searchRequest.js
@@ -1,39 +1,48 @@
 function selectItem(id) {
-    var idInt = parseInt(id)
-    console.log(typeof(idInt))
-    switch (idInt) {
-        case 1:
-            enableForm('searchUser')
-            break;
-        case 2:
-            enableForm('searchDate')
-            break;
-        case 3:
-            enableForm('searchId')
-            break;
-        case 4:
-            enableForm('searchDateEx')
-            break;
-        case 5:
-            enableForm('searchType')
-            break;
-        case 6:
-            enableForm('searchTypeBuidding')
-            break;
-        default:
-            enableForm('searchAddress')
-    }
+  var idInt = parseInt(id);
+  console.log(typeof idInt);
+  switch (idInt) {
+    case 1:
+      enableForm("searchUser");
+      break;
+    case 2:
+      enableForm("searchDate");
+      break;
+    case 3:
+      enableForm("searchId");
+      break;
+    case 4:
+      enableForm("searchPerID");
+      break;
+    case 5:
+      enableForm("searchDateEx");
+      break;
+    case 6:
+      enableForm("searchType");
+      break;
+    case 7:
+      enableForm("searchTypeBuidding");
+      break;
+    case 8:
+      enableForm("searchAddress");
+      break;
+    case 9:
+      enableForm("searchPlace");
+      break;
+  }
 }
 function enableForm(id) {
-    disableForm()
-    document.getElementById(id).classList.remove('displaynone')
+  disableForm();
+  document.getElementById(id).classList.remove("displaynone");
 }
 function disableForm() {
-    document.getElementById('searchUser').classList.add('displaynone')
-    document.getElementById('searchDate').classList.add('displaynone')
-    document.getElementById('searchId').classList.add('displaynone')
-    document.getElementById('searchDateEx').classList.add('displaynone')
-    document.getElementById('searchType').classList.add('displaynone')
-    document.getElementById('searchTypeBuidding').classList.add('displaynone')
-    document.getElementById('searchAddress').classList.add('displaynone')
+  document.getElementById("searchUser").classList.add("displaynone");
+  document.getElementById("searchDate").classList.add("displaynone");
+  document.getElementById("searchId").classList.add("displaynone");
+  document.getElementById("searchPerID").classList.add("displaynone");
+  document.getElementById("searchDateEx").classList.add("displaynone");
+  document.getElementById("searchType").classList.add("displaynone");
+  document.getElementById("searchTypeBuidding").classList.add("displaynone");
+  document.getElementById("searchAddress").classList.add("displaynone");
+  document.getElementById("searchPlace").classList.add("displaynone");
 }
