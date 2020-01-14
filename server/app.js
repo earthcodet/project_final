18c7ad17b4716d389d8c2e02a257da16d77caa02
heroserth2 <59160969@go.buu.ac.th>
2020-01-14 14:30:44 +0700

    create router add all path

--- server/app.js
+++ server/app.js
@@ -1,31 +1,74 @@
-var url = require('url')
-var fs = require('fs')
-function renderHTML(path, resoponse){
-    fs.readFile(path, null, function(err,data) {
-        if(err){
-            resoponse.writeHead(404);
-            resoponse.write('File not found!');
-        }else{
-            resoponse.write(data)
-        }
-        resoponse.end()
-    });
-}
-module.exports ={
-    handleRequest: function(request, response) {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        var path = url.parse(request.url).pathname;
-        switch(path) {
-            case '/' :
-                renderHTML('./client/html/utilities/login.html',response)
-                break;
-            case '/login':
-                renderHTML('./client/html/utilities/login.html',response)
-                break;
-            default:
-                response.writeHead(404)
-                response.write('Route not defind')
-                response.end()
-        }
-    }
-}
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    console.log(path.__dirname)
+    res.sendFile(path.join(__dirname + '/views/html/utilities/login.html'));
+});
+
+// เมนูคำขอ
+router.get('/request/01', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/request_public_sell.html'));
+});
+router.get('/request/02', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/request_public_hawk.html'));
+});
+router.get('/request/03', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/request_area_more_sell.html'));
+});
+router.get('/request/04', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/request_area_more_correct.html'));
+});
+router.get('/request/05', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/request_area_less_sell.html'));
+});
+router.get('/request/06', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/request_area_less_correct.html'));
+});
+router.get('/request/07', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/request_market.html'));
+});
+router.get('/request/08', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/request_health_danger.html'));
+});
+router.get('/request/09', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/request_crematory.html'));
+});
+router.get('/request/search', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/request/searchRequest.html'));
+});
+
+//เมนูส่วนกลาง
+router.get('/operator/01', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/central/operator.html'));
+});
+router.get('/operator/02', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/central/02.html'));
+});
+router.get('/operator/03', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/central/03.html'));
+});
+
+//เมนูรายงาน
+router.get('/report/01', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/report/report_crematory.html'));
+});
+router.get('/report/02', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/report/report_crematory.html'));
+});
+
+//เมนูแจ้งเตือน
+router.get('/notfication', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/html/notfication/request.html'));
+});
+
+
+//ทำให้ css กับ js ใช้ได้
+app.use(express.static(__dirname + '/views'));
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
--- server/server.js
+++ server/server.js
@@ -1,23 +1,101 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
-
-router.get('/',function(req,res){
-    console.log(path.__dirname)
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
-
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
-});
-
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
-});
+var express = require('express');
+const port = 5000;
+const WebDAO = require("./DAO/WebDAO");
+const WebDAOObj = new WebDAO();
+const cors = require('cors')
+var app = express();
+app.use(cors())
+// app.use(session({
+// 	secret: 'secret',
+// 	resave: true,
+// 	saveUninitialized: true
+// }));
 
 
-app.use('/', router);
-app.listen(process.env.port || 3000);
+// app.get("/get", (req, res) => {
+//     WebDAOObj.get().then(data => {
+//       if (data != null) {
+//         res.json(data);
+//         console.log("result :")
+//         console.log(data[0].formId)
+//       } else {
+//         res.sendStatus(404);
+//       }
+//     });
+//   });
 
-console.log('Running at Port 3000');
+  app.get("/get/provice", (req, res) => {
+    WebDAOObj.getProvince().then(data => {
+      if (data != null) {
+        res.json(data);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+  });
+  app.get("/get/amphur", (req, res) => {
+    WebDAOObj.getAmphur().then(data => {
+      if (data != null) {
+        res.json(data);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+  });
+  app.get("/get/district", (req, res) => {
+    WebDAOObj.getDistrict().then(data => {
+      if (data != null) {
+        res.json(data);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+  });
+  // app.get("/get/amphur/:proviceId", (req, res) => {
+  //   WebDAOObj.getAmphur(req.params.proviceId).then(data => {
+  //     if (data != null) {
+  //       res.json(data);
+  //     } else {
+  //       res.sendStatus(404);
+  //     }
+  //   });
+  // });
+  // app.get("/get/district/:proviceId/:amphurId", (req, res) => {
+  //   WebDAOObj.getDistrict(req.params.proviceId,req.params.amphurId).then(data => {
+  //     if (data != null) {
+  //       res.json(data);
+  //     } else {
+  //       res.sendStatus(404);
+  //     }
+  //   });
+  // });
+
+// app.post("/insert", (req, res) => {
+//     WebDAOObj.getAmphur(req.body.formData).then(data => {
+//       console.log(req.body.formData)
+//       res.json(data);
+    
+//     });
+//   });
+//   console.log("pass")
+//   app.post("/update", (req, res) => {
+//     WebDAOObj.update(req.body.formDatas).then(data => {
+//       res.json(data);
+    
+//     });
+//   });
+
+app.listen(port, () => {
+    console.log(`App listening on ${port}`);
+  });
+
+// var username = 'userTest01'
+// var password =  'p123456'
+// con.query('SELECT * FROM user WHERE userId = ? AND password = ?', [username, password], function(error, results) {
+//     console.log(results)
+//     if (results.length > 0) {
+//        console.log('correct Username and password')
+//     } else {
+//         response.send('Incorrect Username and/or Password!');
+//     }			
+// });
