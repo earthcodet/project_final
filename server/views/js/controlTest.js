633bf05182bc1506e546214d7cffd8bbc1b79e17
heroserth2 <59160969@go.buu.ac.th>
2020-01-21 10:30:42 +0700

    edit ui search

--- server/views/js/controlTest.js
+++ server/views/js/controlTest.js
@@ -341,6 +341,114 @@ function searchOparator() {
     }
 
 }
+function searchOparatorFor02() {
+        var swal_html = `<div >
+        <div class="display-center">
+                    <h5 style="font-size: 100%;">
+                        ชื่อ :
+                        <input type="text" id="username" style="width: 18%;">
+                        นามสกุล :
+                        <input type="text" id="userlastname" style="width: 18%;" >
+                        เลขบัตรประจำตัว :
+                        <input type="text" id="userid" style="width: 18%;" >
+                        <button type="button" style="width: auto;height: auto;"
+                        class="btn btn-secondary is-color" >
+                           
+                                <i class="fa fa-search"></i> 
+                                ค้นหา
+                           
+                        </button>
+                    </h5>
+                </div>
+        <div class="search-popup-height">
+            <table id='resultItem' class="table tablesearch table-hover cursor-pointer">
+                <thead>
+                  <tr class="is-color ">
+                    <th>ชื่อ</th>
+                    <th>นามสกุล</th>
+                    <th>ที่อยู่</th>
+                    <th>เลขบัตรประจำตัว</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                
+                <tr ">
+                    <td>นายสมหมาย</td>
+                    <td>จงรัก</td>
+                    <td>169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131</td>
+                    <td>1011122111111</td>
+                </tr>
+                </tbody>
+              </table>
+        </div>
+    </div>`
+
+        Swal.fire({
+            title: "ค้นหารายชื่อผู้ประกอบการ",
+            html: swal_html,
+            width: '80%',
+            customClass: 'swal-height',
+            showConfirmButton: false,
+            closeOnConfirm: false,
+            closeOnCancel: false
+        });
+    
+
+}
 function onSelectUser() {
     Swal.close()
     document.getElementById('typeUser').value = listDataTest.userType
