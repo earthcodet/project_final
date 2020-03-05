af5f3d76f548def10b69bd92f2b9a48b05e324e9
heroserth2 <59160969@go.buu.ac.th>
2020-03-05 11:57:10 +0700

    time out = 0

--- server/DAO/PrintDAO.js
+++ server/DAO/PrintDAO.js
@@ -3,7 +3,8 @@ var con = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: '',
-    database: 'web_database'
+    database: 'web_database',
+    timeout: 0
 });
 class PrintDAO {
     getViewImage(id, year) {
